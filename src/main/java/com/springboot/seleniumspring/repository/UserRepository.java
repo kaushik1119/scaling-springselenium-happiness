991c0b0c1fff8e4a3c6785f80f3627351be4b59b
Kaushik <kaushik.subramanian@ul.com>
2020-12-25 16:31:25 -0500

    run tests using dataprovider and repository, only cucumber portion left in the course

--- src/main/java/com/springboot/seleniumspring/repository/UserRepository.java
+++ src/main/java/com/springboot/seleniumspring/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.springboot.seleniumspring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
 import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findByFromCountry(String country);
+    List<User> findByDobBetween(Date from, Date to);
 }
--- src/test/java/com/springboot/seleniumspring/SpringdataJPATest.java
+++ src/test/java/com/springboot/seleniumspring/SpringdataJPATest.java
@@ -1,8 +1,13 @@
 package com.springboot.seleniumspring;
 
+import com.springboot.seleniumspring.entity.User;
 import com.springboot.seleniumspring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.sql.Date;
 
 public class SpringdataJPATest extends SpringBaseTestNG {
 
@@ -21,5 +26,34 @@ public class SpringdataJPATest extends SpringBaseTestNG {
                 .get(0)
                 .getFirstName()
         );
+    }
+
+    @Test(dataProvider = "getData")
+    public void testFromDataProvider(User user){
+        System.out.println(
+                user.getFirstName() + " " +
+                user.getLastName() + " was born on " +
+                        user.getDob()
+        );
+
+    }
+
+    @DataProvider
+    public Object[] getData(){
+        return userRepository.findByDobBetween(Date.valueOf("1995-01-01"), Date.valueOf("1999-01-01"))
+                .stream()
+                .limit(3)
+                .toArray();
+    }
+
+    @DataProvider
+    public Object[] getDataFromTestNgXml(ITestContext ctx){
+        //create parameters dobFrom and dobTo in the test xml to access them here
+        return userRepository.findByDobBetween(
+                Date.valueOf(ctx.getCurrentXmlTest().getParameter("dobFrom")),
+                Date.valueOf(ctx.getCurrentXmlTest().getParameter("dobTo")))
+                .stream()
+                .limit(3)
+                .toArray();
     }
 }
